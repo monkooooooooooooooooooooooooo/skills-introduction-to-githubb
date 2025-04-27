1053d34dfe4ba26fd1af0dd6d8fdcf95602c7c64
monkooooooooooooooooooooooooo <Edicarlospires@gmail.com>
2025-04-27 13:00:58 -0300

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const config = require('./config.json'); // loads token safely from config.json
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.content === "!ping") {
+        await message.reply("Pong!");
+    }
+});
+
+client.login(config.token);
