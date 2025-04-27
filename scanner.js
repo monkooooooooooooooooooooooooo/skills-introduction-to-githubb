c21772d9a1db5fd3d8a66af8b6a2f044bb344ef2
monkooooooooooooooooooooooooo <Edicarlospires@gmail.com>
2025-04-27 17:29:55 -0300

    scanner.js

--- scanner.js
+++ scanner.js
@@ -1,1 +1,11 @@
+module.exports = { scanUsers };
+if (message.content.startsWith('!scan')) {
+  await message.reply('Starting scan...');
 
+  // Get user IDs from the message (optional)
+  const userIds = [userIds]; // <-- you can hardcode or later make it dynamic
+  
+  await scanner.scanUsers(userIds);
+
+  await message.reply('Scan complete.');
+}
